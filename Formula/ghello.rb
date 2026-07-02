92a69ba43cc4feeae0e14fc38b1c4a90d90e663a
Matt Guest <mjcguest@gmail.com>
2026-07-02 16:00:35 +0100

    ready for v1.4.0

--- Formula/ghello.rb
+++ Formula/ghello.rb
@@ -1,8 +1,8 @@
 class Ghello < Formula
   desc "Tyler Framewok Hello World"
   homepage "https://github.com/ki-aura/ghello"
-  url "https://github.com/ki-aura/ghello/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "c8e2d894f77a38ff8d263fd3f2c669465c77eddf33ef8295423b8cd4e5f46dae"
+  url "https://github.com/ki-aura/ghello/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "fb2682b5186df216dbaaa88c46dfc7ceab55b796c39d7d629fa3179af17d1eb7"
   license "MIT"
 
   def install
